4deb97ff48dfcb48ba0f2b785be921b93f98059b
Disthinker <2511366326@qq.com>
2026-06-27 14:17:06 +0800

    fix: complete gameplay world skeleton

--- src/gameplay_world.cpp
+++ src/gameplay_world.cpp
@@ -1,5 +1,10 @@
 #include "gameplay_world.h"
 
+GameplayWorld::GameplayWorld()
+{
+    enemies_.emplace_back(Vec2{600.0f, 100.0f}, Vec2{50.0f, 50.0f});
+}
+
 void GameplayWorld::update(const GameplayInput &input, float deltaTime)
 {
 }
--- tests/test_gameplay_world.cpp
+++ tests/test_gameplay_world.cpp
@@ -3,27 +3,33 @@
 #include "gameplay_world.h"
 
 // 初始 Player 位置是 (640, 360)
-TEST(gameplayWorldTest, initialPlayerPosition)
+TEST(GameplayWorldTest, InitialPlayerPosition)
 {
     GameplayWorld world;
-    Vec2 expectedPosition{640.0f, 360.0f};
-    EXPECT_EQ(world.player().position(), expectedPosition);
+    const Vec2 position = world.player().position();
+
+    EXPECT_FLOAT_EQ(position.x, 640.0f);
+    EXPECT_FLOAT_EQ(position.y, 360.0f);
 }
 
 // 初始 Projectile 集合为空
-TEST(gameplay_WorldTest, initialProjectilesEmpty)
+TEST(Gameplay_WorldTest, InitialProjectilesEmpty)
 {
     GameplayWorld world;
     EXPECT_TRUE(world.projectiles().empty());
 }
 
 // 初始 Enemy 有 1 个，position=(600,100)，size=(50,50)
-TEST(gameplay_WorldTest, initialEnemiesNotEmpty)
+TEST(Gameplay_WorldTest, InitialEnemiesState)
 {
     GameplayWorld world;
-    Vec2 expectedPosition{600.0f, 100.0f};
-    Vec2 expectedSize{50.0f, 50.0f};
-    EXPECT_FALSE(world.enemies().empty());
-    EXPECT_EQ(world.enemies()[0].position(), expectedPosition);
-    EXPECT_EQ(world.enemies()[0].size(), expectedSize);
+    ASSERT_EQ(world.enemies().size(), 1u);
+
+    const Enemy &enemy = world.enemies()[0];
+    const Vec2 enemyPosition = enemy.position();
+    const Vec2 enemySize = enemy.size();
+    EXPECT_FLOAT_EQ(enemyPosition.x, 600.0f);
+    EXPECT_FLOAT_EQ(enemyPosition.y, 100.0f);
+    EXPECT_FLOAT_EQ(enemySize.x, 50.0f);
+    EXPECT_FLOAT_EQ(enemySize.y, 50.0f);
 }
